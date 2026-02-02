21886d133b801e3a98157a03a1be25430df145b4
Teppei Tazawa <kisuke.luna@gmail.com>
2026-02-02 17:35:03 +0900

    Quartz sync: Feb 2, 2026, 5:35 PM

--- quartz/components/PopularTags.tsx
+++ quartz/components/PopularTags.tsx
@@ -1,40 +1,16 @@
-import { QuartzComponent, QuartzComponentConstructor } from "./types"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
 
-const MAX_TAGS = 10
-const MIN_COUNT = 2 // 1回しか使われていないタグは表示しない
-
-const PopularTags: QuartzComponent = ({ allFiles }) => {
-  const tagCount: Record<string, number> = {}
-
-  for (const file of allFiles) {
-    const tags = file.frontmatter?.tags
-    if (!tags) continue
-
-    for (const tag of tags) {
-      tagCount[tag] = (tagCount[tag] ?? 0) + 1
-    }
+export default (() => {
+  const PopularTags: QuartzComponent = ({ allFiles, displayClass }: QuartzComponentProps) => {
+    return (
+      <div className={classNames(displayClass, "popular-tags")}>
+        <hr />
+        <h3 style={{ color: "red" }}>テスト表示：人気のタグ</h3>
+        <p>記事の数: {allFiles.length}</p>
+      </div>
+    )
   }
 
-  const popularTags = Object.entries(tagCount)
-    .filter(([, count]) => count >= MIN_COUNT)
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, MAX_TAGS)
-
-  if (popularTags.length === 0) return null
-
-  return (
-    <div class="popular-tags">
-      <h3>Popular Tags</h3>
-      <ul>
-        {popularTags.map(([tag, count]) => (
-          <li>
-            <a href={`/tags/${tag}`}>#{tag}</a>
-            <span class="count">{count}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-export default (() => PopularTags) satisfies QuartzComponentConstructor
+  return PopularTags
+}) satisfies QuartzComponentConstructor
